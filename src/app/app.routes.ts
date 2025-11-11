b264611cee34e27195b992d54eecadd8485e06e4
Jose <utti222001@utvco.edu.mx>
2025-11-11 10:35:26 -0600

    Login creado y establecido como ruta por defecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,20 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'nuevo',
+    loadComponent: () => import('./pages/registrar/registrar.page').then( m => m.RegistrarPage)
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+
 ];
