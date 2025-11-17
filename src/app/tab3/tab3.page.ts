1dc8f7b70623fbd39db27ea867b5b644ea37bf38
Mildred Alely cruz Juarez <mildredalely07@gmail.com>
2025-11-16 18:14:07 -0600

    agregando el footer

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardHeader, IonCard } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { ReporteComponent } from '../reporte/reporte.component';
+import { FooterComponent } from '../components/footer/footer.component';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [ReporteComponent],
+  imports: [ReporteComponent, FooterComponent],
 })
 export class Tab3Page {
   constructor() {}
