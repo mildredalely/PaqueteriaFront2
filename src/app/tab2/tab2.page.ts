6858b8ee26d8db25c707f9684ad2c5811bee2d7f
Miriam Belen <miriamstgos@gmail.com>
2025-11-22 13:27:04 -0600

    corrigiendo tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { HistorialPage } from '../pages/historial/historial.page';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule, HistorialPage]
+  imports: [CommonModule, IonicModule, HistorialPage,]
 })
 export class Tab2Page {
   constructor() {}
