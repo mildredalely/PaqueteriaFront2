90b6d23f062c7a6c1df991c8e6c8be209ab83471
Jose <utti222001@utvco.edu.mx>
2025-12-10 01:38:43 -0600

    logout funcionando

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { IonButton, IonFooter, IonToolbar, IonIcon, IonButtons } from "@ionic/angular/standalone";
-
+import { Observable } from 'rxjs';
+import { Conection } from 'src/app/services/conection';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -10,8 +11,23 @@ import { IonButton, IonFooter, IonToolbar, IonIcon, IonButtons } from "@ionic/an
 })
 export class FooterComponent  implements OnInit {
 
-  constructor() { }
+  constructor(
+    private conection: Conection,
+    private router: Router,
+  ) { }
 
   ngOnInit() {}
 
+  logout(): Observable<any> {
+    // Limpiar datos locales
+    localStorage.removeItem('token');
+    sessionStorage.removeItem('token');
+    
+    // Retornar un Observable vacío o hacer una llamada simbólica
+    this.router.navigate(['/login'], { });
+    return new Observable(observer => {
+      observer.next({ success: true });
+      observer.complete();
+    });
+  }
 }
